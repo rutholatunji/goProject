f95478578f17ae01c0bb31280fa40b7cb0a32ab5
Ruth Olatunji <ruth.o2009@live.co.uk>
2020-12-24 16:23:37 +0000

    Comic Book exercise

---
+++ comicBook.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func main() {
+	var publisher, writer, artist, title string
+	title = "Mr. GoToSleep"
+	writer = "Tracey Hatchet"
+	artist = "Jewel Tampson"
+	publisher = "DizzyBooks Publishing Inc."
+
+	var year, pageNumber int
+	year = 1997
+	pageNumber = 14
+
+	var grade float32
+	grade = 6.5
+
+	fmt.Println(title, "written by", writer, "drawn by", artist, "published by", publisher, "Year:", year, "Page numbers:", pageNumber, "Grade:", grade)
+
+	title = "Epic Vol.1"
+	writer = "Ryan N. Shawn"
+	artist = "Phoebe Paperclips"
+	publisher = "DizzyBooks Publishing Inc."
+	year = 2013
+	pageNumber = 160
+	grade = 9.0
+
+	fmt.Println(title, "written by", writer, "drawn by", artist, "published by", publisher, ",Year:", year, ",Page numbers:", pageNumber, ",Grade:", grade)
+
+}
